1d60483c6b830ef07b12c9ba9dc4c7f807bd67a5
lawki <prakharpalpfdr+github@gmail.com>
2019-09-16 01:07:44 +0530

    initial draft of PopupTextarea

---
+++ src/components/PopupTextarea/index.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import {  Modal, Button, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { BaseFieldProps } from '../../common/interfaces/CommonProps';
+
+export interface PopupTextareaProps extends BaseFieldProps{
+    defaultValue?: string;    
+    buttonLabel: string;
+    buttonColor?: string;
+    modalClassName?: string;
+    modalTitle: any;
+    modalBody: string;
+    cancelButtonText: string;
+    doneButtonText: string;
+}
+
+export interface PopupTextareaState{
+    isOpen: boolean;
+} 
+
+export class RFInput extends React.Component<PopupTextareaProps, any>{
+
+    static defaultProps = {
+        buttonColor: 'danger'
+    }
+
+    constructor(props: PopupTextareaProps){
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    toggle = ()=> this.setState((oldState: PopupTextareaState)=>({isOpen: !oldState.isOpen}));
+
+
+    render(){
+        const { buttonLabel, buttonColor, modalClassName, modalTitle,
+             doneButtonText, cancelButtonText, input} = this.props;
+        const { isOpen } = this.state;
+        return (
+            <React.Fragment>
+                <Button color={buttonColor} onClick={this.toggle}>{buttonLabel}</Button>
+                <Modal isOpen={isOpen} toggle={this.toggle} className={modalClassName}>
+                    <ModalHeader toggle={this.toggle}>{modalTitle}</ModalHeader>
+                    <ModalBody>
+                        {}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>{doneButtonText}</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>{cancelButtonText}</Button>
+                    </ModalFooter>
+                </Modal>
+            </React.Fragment>
+        )
+    }
+}
