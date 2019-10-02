479006cf2abbca5279bc5b302a7d075ff2e276d9
lawki <prakharpalpfdr+github@gmail.com>
2019-10-02 21:15:29 +0530

    add FormComponent to handle common tasks of all components and use it for Input Component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,45 +1,32 @@
 import * as React from 'react';
 import { Input as RsInput, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
-import isEqual from 'lodash.isequal';
 import { BaseFieldProps } from '../../common/interfaces/CommonProps';
 import { Label } from '../Label';
+import FormComponent from '../FormComponent';
 
-export interface InputProps extends BaseFieldProps{
+export interface InputProps {
     type: string;
-    defaultValue?: any;
+    value?: any;
     addons?: {
         prepend: string[],
         append: string[]
     };
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onBlur?: ((event: React.FocusEvent<HTMLInputElement>) => void) | undefined;
+    noLabel?: boolean;
+    label?: string | { htmlFor: string, value: any }
+    className?: string;
 }
 
-export class Input extends React.Component<InputProps, any>{
-
-    componentDidMount(){
-        const { defaultValue } = this.props;
-        if(defaultValue !== undefined) this.saveToStore(defaultValue);
-    }
-
-    componentDidUpdate(oldProps: InputProps){
-        const { defaultValue, onSave, input } = this.props;
-        if(!isEqual(oldProps.defaultValue, defaultValue)) this.saveToStore(defaultValue);
-        if(!isEqual(oldProps.input.value, input.value) && onSave && typeof onSave === typeof Function) 
-            onSave(input.value);
-    }
+export default class InputBS extends React.Component<InputProps, any>{
 
     affixData = (data: string[], type: "prepend" | "append") => (data && Array.isArray(data) ?
         (<InputGroupAddon addonType={type}>
             {data.map((d, index) => <InputGroupText key={index}>{d}</InputGroupText>)}
         </InputGroupAddon>) : null);
 
-    saveToStore = (data: any)=>{
-        const { filter, input } = this.props;
-        data = filter && typeof filter === typeof Function? filter(data): data;
-        input.onChange(data);
-    }
-
-    render(){
-        const { type, defaultValue, addons, input, label, noLabel, outermostClass, fieldClass, ...rest } = this.props;
+    render() {
+        const { type, addons, label, noLabel, value, className, ...rest } = this.props;
         return (
             <React.Fragment>
                 {!noLabel && label ? <Label value={label} /> : null}
@@ -47,14 +34,37 @@ export class Input extends React.Component<InputProps, any>{
                     {addons && this.affixData(addons.prepend, "prepend")}
                     <RsInput
                         type={type as any}
-                        value={input.value}
-                        className={fieldClass}
+                        value={value}
+                        className={className}
                         {...rest}
-                        {...input}
                     />
                     {addons && this.affixData(addons.append, "append")}
                 </InputGroup>
             </React.Fragment>
         )
+    }
+}
+
+
+export type InputFormType = InputProps & BaseFieldProps;
+export class Input extends FormComponent<InputFormType>{
+    constructor(props: InputFormType, state: any) {
+        super(props, state);
+    }
+    handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        this.saveToStore(value);
+    }
+    render() {
+        const { type, addons, label, value, noLabel, ...rest } = this.props;
+        return <InputBS
+            type={type}
+            addons={addons}
+            label={label}
+            noLabel={noLabel}
+            value={value}
+            onChange={this.handleOnChange}
+            {...rest}
+        />
     }
 }
