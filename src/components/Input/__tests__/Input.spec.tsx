b500a2f520b2a4de2cb1c1b33ad49b68b3047496
lawki <prakharpalpfdr+github@gmail.com>
2019-10-02 22:51:23 +0530

    add tests to input component and update snapshot

--- src/components/Input/__tests__/Input.spec.tsx
+++ src/components/Input/__tests__/Input.spec.tsx
@@ -1,9 +1,32 @@
 import * as React from "react";
 import { shallow } from "enzyme";
-import { Input } from "..";
+import { Input as RSInput } from 'reactstrap';
+import Input  from "..";
 describe("Input", () => {
+
   it('should render correctly', () => {
-    const component = shallow(<Input />);
+    const fn = jest.fn();
+    const component = shallow(<Input type="text" onChange={fn} />);
     expect(component).toMatchSnapshot();
   });
-});
+
+  it('has only one instance of reactstrap\'s Input ', () => {
+    const fn = jest.fn();
+    const app = shallow(<Input type="text" onChange={fn}/>);
+    expect(app.find(RSInput).length).toBe(1);
+  });
+
+  it('calls change event handler', ()=>{
+    const fn = jest.fn();
+    const app = shallow(<Input type="text" onChange={fn}/>);
+    app.find(RSInput).simulate('change', { target: { value: 30} });
+    expect(fn.call.length).toBe(1);
+  });
+
+  it('set value correctly',()=>{
+    const fn = jest.fn();
+    const app = shallow(<Input type="text" onChange={fn} value={30}/>);
+    app.find(RSInput).simulate('change', { target: { value: 30} });
+    expect(fn.call.length).toBe(1);
+  })
+})
