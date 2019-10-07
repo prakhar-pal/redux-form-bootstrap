ba511c481af4567ff4c45a1e1c57ffc7bb2df32c
lawki <prakharpalpfdr+github@gmail.com>
2019-10-07 16:42:17 +0530

    [feature] direction(horizontal/vertical) to display radio options

--- src/components/RadioButtons/index.tsx
+++ src/components/RadioButtons/index.tsx
@@ -1,33 +1,52 @@
 import * as React from 'react';
-import { Input, InputGroup } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 import FormComponent from '../FormComponent';
 import { BaseFieldProps } from '../../common/interfaces/CommonProps';
 import { Label } from '../Label';
+
+export const RADIO_BUTTONS_DIRECTIONS = {
+    HORIZONTAL: "horizontal",
+    VERTICAL: "vertical"
+}
 export interface RadioButtonsProps {
     options: string[];
     noLabel?: boolean;
-    label: string | { htmlFor: string, value: any }
+    label?: string | { htmlFor: string, value: any }
     value?: string;
     onChange: (value: string) => void;
+    direction?: "horizontal" | "vertical"
 }
 export default class RadioButtonsBS extends React.Component<RadioButtonsProps, any>{
+    static defaultProps = {
+        direction: "vertical"
+    }
     render() {
-        const { options, noLabel, label, onChange, value, ...rest } = this.props;
+        const { options, noLabel, label, onChange, value, direction, ...rest } = this.props;
+        const radioButtons = options && Array.isArray(options) ?
+            options.map((option, index) => (
+                <Row className={classnames({ "ml-4": index != 0 && direction !== RADIO_BUTTONS_DIRECTIONS.VERTICAL })}>
+                    <div key={option} >
+                        <input type="radio" value={option}
+                            checked={value === option}
+                            {...rest}
+                            onClick={() => onChange(option)}
+                        />
+                    </div>
+                    <Label value={option} className="ml-1"
+                        onClick={() => onChange(option)} />
+                </Row>)) : null;
         return (
             <React.Fragment>
                 {!noLabel && label ? <Label value={label} /> : null}
-                {options && Array.isArray(options) ?
-                    options.map(option => (<>
-                        <InputGroup>
-                            <Input type="radio" value={option}
-                                checked={value === option}
-                                onClick={() => onChange(option)}
-                                {...rest}
-                            />
-                        </InputGroup>
-                        <Label value={option} />
-                    </>)) : null
-                }
+                {direction === RADIO_BUTTONS_DIRECTIONS.HORIZONTAL ? (
+                    <Row className="ml-3">
+                        {radioButtons}
+                    </Row>) : (
+                        <Col>
+                            {radioButtons}
+                        </Col>
+                    )}
             </React.Fragment>
         )
     }
@@ -36,7 +55,7 @@ export default class RadioButtonsBS extends React.Component<RadioButtonsProps, a
 export type RadioButtonsFormProps = RadioButtonsProps & BaseFieldProps;
 export class RadioButtons extends FormComponent<RadioButtonsFormProps>{
     render() {
-        const { options, noLabel, label, value } = this.props;
+        const { options, noLabel, label, direction, input: { value } } = this.props;
         return (
             <RadioButtonsBS
                 options={options}
@@ -44,6 +63,7 @@ export class RadioButtons extends FormComponent<RadioButtonsFormProps>{
                 label={label}
                 value={value}
                 onChange={this.saveToStore}
+                direction={direction}
             />
         )
     }
