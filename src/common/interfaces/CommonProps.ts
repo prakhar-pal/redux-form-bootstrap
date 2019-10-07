52cea82a763cf98b574a68fb9be0ccec1db01700
lawki <prakharpalpfdr+github@gmail.com>
2019-10-07 16:40:51 +0530

    add className and onClick as prop to Label component

--- src/common/interfaces/CommonProps.ts
+++ src/common/interfaces/CommonProps.ts
@@ -3,6 +3,8 @@ import { WrappedFieldProps} from 'redux-form';
 export interface LabelObject { htmlFor: string, value: any }
 export interface LabelProps {
     value: string | LabelObject;
+    className?: string;
+    onClick?: Function;
 }
 export interface BaseFieldProps extends WrappedFieldProps{
     label?: string | LabelObject;
