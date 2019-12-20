317df5c992c8a9f922ddaa0dee5efc1acce65eb3
lawki <prakharpalpfdr+github@gmail.com>
2019-12-21 02:38:01 +0530

    add redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 const reducers = {
     form: formReducer
 }
-export const store = createStore(combineReducers(reducers));
+export const store = createStore(
+    combineReducers(reducers), 
+    compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
