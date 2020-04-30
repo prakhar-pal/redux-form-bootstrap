57e9c635bf72222d24eaf6c19b67139ca39ccb33
lawki <prakharpalpfdr+github@gmail.com>
2020-05-01 00:34:22 +0530

    replace FormComponent by withFormComponent HOC
    also update imports in main index.ts

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Input as RsInput, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
 import { BaseFieldProps } from '../../common/interfaces/CommonProps';
 import { Label } from '../Label';
-import FormComponent from '../FormComponent';
+import withFormComponent, { InjectedExtraProps } from '../../common/withFormComponent';
 
 export interface InputProps {
     type: string;
@@ -11,19 +11,24 @@ export interface InputProps {
         prepend: string[],
         append: string[]
     };
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
     onBlur?: ((event: React.FocusEvent<HTMLInputElement>) => void) | undefined;
     noLabel?: boolean;
     label?: string | { htmlFor: string, value: any }
     className?: string;
 }
 
-export default class InputBS extends React.Component<InputProps, any>{
+class InputComponent extends React.Component<InputProps & InjectedExtraProps, any>{
 
     affixData = (data: string[], type: "prepend" | "append") => (data && Array.isArray(data) ?
         (<InputGroupAddon addonType={type}>
             {data.map((d, index) => <InputGroupText key={index}>{d}</InputGroupText>)}
         </InputGroupAddon>) : null);
+
+    handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        const { onChange } = this.props;
+        onChange(value);
+    }
 
     render() {
         const { type, addons, label, noLabel, value, className, ...rest } = this.props;
@@ -47,24 +52,5 @@ export default class InputBS extends React.Component<InputProps, any>{
 
 
 export type InputFormType = InputProps & BaseFieldProps;
-export class Input extends FormComponent<InputFormType>{
-    constructor(props: InputFormType, state: any) {
-        super(props, state);
-    }
-    handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-        this.saveToStore(value);
-    }
-    render() {
-        const { type, addons, label, value, noLabel, ...rest } = this.props;
-        return <InputBS
-            type={type}
-            addons={addons}
-            label={label}
-            noLabel={noLabel}
-            value={value}
-            onChange={this.handleOnChange}
-            {...rest}
-        />
-    }
-}
+const Input = withFormComponent(InputComponent);
+export default Input;
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
-import InputBS from './components/Input';
-import RadioButtonsBS from './components/RadioButtons';
-import SelectBS from './components/Select';
-
-export { Input , InputProps, InputFormType } from './components/Input';
+export { InputProps, InputFormType } from './components/Input';
+import Input from './components/Input';
 export { Dropdown, DropdownProps } from './components/Dropdown';
 export { Label } from './components/Label';
 export { LabelProps } from './common/interfaces/CommonProps';
 export { PopupTextarea, PopupTextareaProps } from './components/PopupTextarea';
 export { RadioButtons, RadioButtonsProps, RadioButtonsFormProps} from './components/RadioButtons';
 export { Select, SelectFormProps, SelectProps } from './components/Select';
-
-const DisconnectedComponents = {
-    InputBS,
-    RadioButtonsBS,
-    SelectBS
-}
-export default DisconnectedComponents;
+export { Input };
+// export default DisconnectedComponents;
